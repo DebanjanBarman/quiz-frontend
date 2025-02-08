92396964bf02a6e19b50e37b58320eae0db0872d
Debanjan Barman <debanjanbarman123456789@gmail.com>
2025-02-08 09:41:24 +0530

    changes

--- src/pages/quiz.vue
+++ src/pages/quiz.vue
@@ -62,7 +62,7 @@
     <!--    Main section-->
     <div class="main_section">
       <div class="left_section">
-        <MdPreview :editorId="id" :modelValue="active_question.question"/>
+        <MdPreview language="en-US" :editorId="id" :modelValue="active_question.question"/>
         <!--        <MdCatalog :editorId="id" :scrollElement="scrollElement"/>-->
       </div>
       <div class="right_section">
